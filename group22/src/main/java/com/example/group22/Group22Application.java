18fe771e363e87c557848191ad5c9d4679e02d4f
prof3130 <hr606438@dal.ca>
2020-05-24 19:05:41 -0300

    Initial project setup of spring boot

---
+++ group22/src/main/java/com/example/group22/Group22Application.java
@@ -0,0 +1,13 @@
+package com.example.group22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group22Application.class, args);
+	}
+
+}
