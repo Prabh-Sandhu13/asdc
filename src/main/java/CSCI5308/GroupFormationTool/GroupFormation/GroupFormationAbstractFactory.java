5f774afc7b5b2ebfb3e7f488b20d9a09a9a51f35
Padmesh Donthu <pd616769@dal.ca>
2020-07-12 00:53:32 -0300

    Fixing the package change error

--- src/main/java/CSCI5308/GroupFormationTool/GroupFormation/GroupFormationAbstractFactory.java
+++ src/main/java/CSCI5308/GroupFormationTool/GroupFormation/GroupFormationAbstractFactory.java
@@ -1,4 +1,25 @@
 package CSCI5308.GroupFormationTool.GroupFormation;
 
+import java.util.ArrayList;
+
 public class GroupFormationAbstractFactory implements IGroupFormationAbstractFactory {
+    @Override
+    public IGroup createGroupInstance() {
+        return new Group();
+    }
+
+    @Override
+    public ArrayList<IGroup> createGroupListInstance() {
+        return new ArrayList<IGroup>();
+    }
+
+    @Override
+    public IGroupFormationManager createGroupFormationManagerInstance() {
+        return new GroupFormationManager();
+    }
+
+    @Override
+    public GroupFormationRepository createGroupFormationRepositoryInstance() {
+        return new GroupFormationRepository();
+    }
 }
